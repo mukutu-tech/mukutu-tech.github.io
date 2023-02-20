107cabde78d29bdc7cf4dedc2b642c9dfb000490
davidsemakula <hello@davidsemakula.com>
2023-02-20 09:29:03 +0300

    add redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         output: '/sitemap',
         serialize: ({ path, modifiedGmt }) => ({
           url: path,
-          lastmod: modifiedGmt ? modifiedGmt : '2023-02-11',
+          lastmod: modifiedGmt ? modifiedGmt : '2023-02-20',
         }),
       },
     },
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,4 +5,9 @@ exports.createPages = async ({ actions }) => {
     toPath:
       'https://docs.google.com/presentation/d/1kOVK3x1j8AgHMl0xf66MjcZA9xPaRj6zAu2j0cXpF20/edit?usp=sharing',
   });
+  createRedirect({
+    fromPath: '/milestones/moonbeam',
+    toPath:
+      'https://docs.google.com/presentation/d/1KBXr3DxWzgarFv_lj8qFCqcEdedBBTeIvDihb5qAeow/edit?usp=sharing',
+  });
 };
